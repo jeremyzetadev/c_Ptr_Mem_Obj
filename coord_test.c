6873180591aa83945f33ad43f2102a7635de53ad
jeremy zeta <jeremy_zeta@yahoo.com>
2025-05-26 21:09:42 +0900

    Pointer to stack will error

---
+++ coord_test.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+typedef struct{
+    int x;
+    int y;
+} coord_t;
+
+//POINTERS TO THE STACK
+
+//error by CREATING-PTR not in heap
+// coord_t *new_coord_err(int x, int y){
+//     coord_t c;
+//     c.x = x;
+//     c.y = y;
+//     return &c;
+//     // error same output
+//     // compiler knows
+// }
+
+//OK by CREATING-OBJ in stack
+coord_t new_coord(int x, int y){
+    coord_t c;
+    c.x = x;
+    c.y = y;
+    return c;
+}
+
+int main(){
+    // coord_t *c1 = new_coord_err(10, 20);
+    // coord_t *c2 = new_coord_err(30, 40);
+    // coord_t *c3 = new_coord_err(50, 60);
+
+    coord_t c1 = new_coord(10, 20);
+    coord_t c2 = new_coord(30, 40);
+    coord_t c3 = new_coord(50, 60);
+
+
+    printf("c1: %d, %d\n", c1.x, c1.y);
+    printf("c2: %d, %d\n", c2.x, c2.y);
+    printf("c3: %d, %d\n", c3.x, c3.y);
+    return 0;
+}
